6f29b8c11110222bcaeb0d97d1930ebae3a4d06c
asthap1112-lgtm <asthap1112@gmail.com>
2025-12-02 00:36:14 +0530

    Add files via upload

---
+++ model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const bookSchema = new mongoose.Schema({
+    title: {type:String, required: true},
+    author:{type:String, required: true},
+    year:{type:Number},
+    genre:{type:String}
+});
+moule.exports = mongoose.model('book',bookSchema);
